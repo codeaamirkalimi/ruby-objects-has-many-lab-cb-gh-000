af095e00f13ff143fbe2da9d8d54083cf23a7dc5
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 16:19:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,12 @@
 class Post
-  attr_accessor :author
-  attr_reader :title
+	attr_accessor :title, :author
 
-  def initialize(title)
-    @title = title
-  end
+	def initialize(title)
+		@title = title
+		@author = nil
+	end
 
-  def author_name
-    self.author != nil ? self.author.name : nil
-  end
-
+	def author_name
+		@author ? self.author.name : nil
+	end
 end
