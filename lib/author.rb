cd6ba32486b9f88509a50982ae9d76b4ce384f69
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 16:19:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,29 @@
 class Author
-  attr_accessor :name
-  attr_reader :posts
-  @@post_count = 0
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
+	attr_accessor :name, :posts
+	@@post_count = 0
+	def initialize(name)
+		@name = name
+		@posts = []
+		@@post_count
+	end
 
-  def add_post(post)
-    post.author = self
-    @posts << post
+	def add_post(post)
     @@post_count += 1
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
     @posts << post
-    @@post_count +=1
+    post.author = self
+    post
   end
 
-  def self.post_count
-    @@post_count
-  end
+	def add_post_by_title(title)
+		@@post_count += 1
+		post = Post.new(title)
+		@posts << post
+		post.author = self
+ 		post
+	end
+
+	def self.post_count
+		@@post_count
+	end
 
 end
